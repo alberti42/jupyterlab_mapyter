0c75650ec45512323a6f39d734fcb448f66ffabd
Andrea Alberti <a.alberti82@gmail.com>
2024-11-02 00:36:25 +0100

    Works again relying on matlab syntax highlighting extension: jupyter_matlab_labextension

--- src/index.ts
+++ src/index.ts
@@ -1,73 +1,76 @@
 import {
-  JupyterFrontEnd,
-  JupyterFrontEndPlugin
+    JupyterFrontEnd,
+    JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
 import {
-  ICommandPalette
+    ICommandPalette
 } from '@jupyterlab/apputils';
 
-import CodeMirror from 'codemirror';
-import { MATLAB_mode } from './codemirror-matlab';
+import { ILabShell } from '@jupyterlab/application';
+import { IEditorTracker } from '@jupyterlab/fileeditor';
+
+// import { MATLAB_mode } from './codemirror-matlab';
 
 function download_matlab_fig(btn_item: HTMLElement, fmt: string) {
-  let data = "";
-  let download = "";
-  
-  let container = btn_item.nextElementSibling as HTMLElement;
-  
-  switch(fmt) {
+    let data = "";
+    let download = "";
+
+    let container = btn_item.nextElementSibling as HTMLElement;
+
+    switch(fmt) {
     case 'svg':
-      data = "data:image/svg+xml;base64," + 
-      btoa(`<?xml version="1.0"?><!DOCTYPE svg PUBLIC '-//W3C//DTD SVG 1.0//EN' 'http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd'>${container.innerHTML}`);
-      download = 'matlab_graph.svg';
-      break;
+        data = "data:image/svg+xml;base64," + 
+        btoa(`<?xml version="1.0"?><!DOCTYPE svg PUBLIC '-//W3C//DTD SVG 1.0//EN' 'http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd'>${container.innerHTML}`);
+        download = 'matlab_graph.svg';
+        break;
     case 'gif':
     case 'png':
     case 'mp4':
-      data = (container as HTMLVideoElement).src || "";
-      download = `matlab_graph.${fmt}`;
-      break;
+        data = (container as HTMLVideoElement).src || "";
+        download = `matlab_graph.${fmt}`;
+        break;
     case 'mov':
     case 'zip':
-      data = (container as HTMLAnchorElement).href || "";
-      download = `matlab_graph.${fmt}`;
-      break;
-  }
+        data = (container as HTMLAnchorElement).href || "";
+        download = `matlab_graph.${fmt}`;
+        break;
+    }
 
-  let a = document.createElement('a');
-  a.download = download;
-  a.href = data;
-  a.target = '_blank';
+    let a = document.createElement('a');
+    a.download = download;
+    a.href = data;
+    a.target = '_blank';
 
-  a.dispatchEvent(new MouseEvent('click', {
-    view: window,
-    bubbles: false,
-    cancelable: true
-  }));
+    a.dispatchEvent(new MouseEvent('click', {
+        view: window,
+        bubbles: false,
+        cancelable: true
+    }));
 }
 
-
 const plugin: JupyterFrontEndPlugin<void> = {
-  id: 'jupyterlab_mapyter:plugin',
-  autoStart: true,
-  requires: [ICommandPalette],
-  activate: (app: JupyterFrontEnd, palette: ICommandPalette) => {
-    console.log('JupyterLab extension jupyterlab_mapyter is activated!');
+    id: 'jupyterlab_mapyter:plugin',
+    autoStart: true,
+    requires: [ICommandPalette, IEditorTracker, ILabShell],
+    activate: (app: JupyterFrontEnd, palette: ICommandPalette, editorTracker: IEditorTracker, labShell: ILabShell) => {
+        console.log('JupyterLab extension jupyterlab_mapyter is activated!');
 
-    MATLAB_mode(CodeMirror);
-    (window as any).download_matlab_fig = download_matlab_fig;
+        (window as any).download_matlab_fig = download_matlab_fig;
 
-    const command: string = 'matlab:open';
-    app.commands.addCommand(command, {
-      label: 'Test command',
-      execute: () => {
-        console.log('This is a test command for MATLAB in JupyterLab.');
-      }
-    });
+        // Force MATLAB syntax highlighting in open editors with ".m" extension
+        editorTracker.widgetAdded.connect((sender: any, widget: { content: { editor: any; model: any; }; }) => {
+          const editor = widget.content.editor;
+          const model = widget.content.model;
+          
+          if (model && model.path.endsWith('.m')) {
+            editor.setOption('language', 'matlab');
+            console.log('Applied MATLAB syntax highlighting to:', model.path);
+          }
+        });
 
-    palette.addItem({ command, category: 'MATLAB' });
-  }
+        // Additional commands or functionality for your custom kernel can go here
+    }
 };
 
 export default plugin;
