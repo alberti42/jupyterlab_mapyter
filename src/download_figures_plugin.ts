f11d6e8a5b86c11888cf37054b0f9d983d051643
Andrea Alberti <a.alberti82@gmail.com>
2024-11-02 11:42:51 +0100

    Removed handling of prettier in eslint by `eslint-plugin-prettier`

--- src/download_figures_plugin.ts
+++ src/download_figures_plugin.ts
@@ -1,6 +1,6 @@
 import {
     JupyterFrontEnd,
-  JupyterFrontEndPlugin
+    JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
 import { ICommandPalette } from '@jupyterlab/apputils';
@@ -9,70 +9,70 @@ import { ILabShell } from '@jupyterlab/application';
 import { IEditorTracker } from '@jupyterlab/fileeditor';
 
 function download_matlab_fig(btn_item: HTMLElement, fmt: string) {
-  let data = '';
-  let download = '';
+    let data = '';
+    let download = '';
 
-  const container = btn_item.nextElementSibling as HTMLElement;
+    const container = btn_item.nextElementSibling as HTMLElement;
 
-  switch (fmt) {
-    case 'svg':
-      data =
-        'data:image/svg+xml;base64,' +
-        btoa(
-          `<?xml version="1.0"?><!DOCTYPE svg PUBLIC '-//W3C//DTD SVG 1.0//EN' 'http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd'>${container.innerHTML}`
-        );
-      download = 'matlab_graph.svg';
-      break;
-    case 'gif':
-    case 'png':
-    case 'mp4':
-      data = (container as HTMLVideoElement).src || '';
-      download = `matlab_graph.${fmt}`;
-      break;
-    case 'mov':
-    case 'zip':
-      data = (container as HTMLAnchorElement).href || '';
-      download = `matlab_graph.${fmt}`;
-      break;
-  }
+    switch (fmt) {
+        case 'svg':
+            data =
+                'data:image/svg+xml;base64,' +
+                btoa(
+                    `<?xml version="1.0"?><!DOCTYPE svg PUBLIC '-//W3C//DTD SVG 1.0//EN' 'http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd'>${container.innerHTML}`
+                );
+            download = 'matlab_graph.svg';
+            break;
+        case 'gif':
+        case 'png':
+        case 'mp4':
+            data = (container as HTMLVideoElement).src || '';
+            download = `matlab_graph.${fmt}`;
+            break;
+        case 'mov':
+        case 'zip':
+            data = (container as HTMLAnchorElement).href || '';
+            download = `matlab_graph.${fmt}`;
+            break;
+    }
 
-  const a = document.createElement('a');
-  a.download = download;
-  a.href = data;
-  a.target = '_blank';
+    const a = document.createElement('a');
+    a.download = download;
+    a.href = data;
+    a.target = '_blank';
 
-  a.dispatchEvent(
-    new MouseEvent('click', {
-      view: window,
-      bubbles: false,
-      cancelable: true
-    })
-  );
+    a.dispatchEvent(
+        new MouseEvent('click', {
+            view: window,
+            bubbles: false,
+            cancelable: true
+        })
+    );
 }
 
 export const downloadFiguresPlugin: JupyterFrontEndPlugin<void> = {
-  id: 'jupyterlab_mapyter:plugin',
-  autoStart: true,
-  requires: [ICommandPalette, IEditorTracker, ILabShell],
-  activate: (
-    app: JupyterFrontEnd,
-    palette: ICommandPalette,
-    editorTracker: IEditorTracker,
-    labShell: ILabShell
-  ) => {
-    console.log('JupyterLab extension jupyterlab_mapyter is activated!');
+    id: 'jupyterlab_mapyter:plugin',
+    autoStart: true,
+    requires: [ICommandPalette, IEditorTracker, ILabShell],
+    activate: (
+        app: JupyterFrontEnd,
+        palette: ICommandPalette,
+        editorTracker: IEditorTracker,
+        labShell: ILabShell
+    ) => {
+        console.log('JupyterLab extension jupyterlab_mapyter is activated!');
 
-    (window as any).download_matlab_fig = download_matlab_fig;
+        (window as any).download_matlab_fig = download_matlab_fig;
 
-    // // Force MATLAB syntax highlighting in open editors with ".m" extension
-    // editorTracker.widgetAdded.connect((sender: any, widget: { content: { editor: any; model: any; }; }) => {
-    //   const editor = widget.content.editor;
-    //   const model = widget.content.model;
+        // // Force MATLAB syntax highlighting in open editors with ".m" extension
+        // editorTracker.widgetAdded.connect((sender: any, widget: { content: { editor: any; model: any; }; }) => {
+        //   const editor = widget.content.editor;
+        //   const model = widget.content.model;
 
-    //   if (model && model.path.endsWith('.m')) {
-    //     editor.setOption('language', 'matlab');
-    //     console.log('Applied MATLAB syntax highlighting to:', model.path);
-    //   }
-    // });
-  }
+        //   if (model && model.path.endsWith('.m')) {
+        //     editor.setOption('language', 'matlab');
+        //     console.log('Applied MATLAB syntax highlighting to:', model.path);
+        //   }
+        // });
+    }
 };
